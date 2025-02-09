8d5ac9746082dd719b1838383dede8efc86d6973
ych <joatroyue@163.com>
2025-02-09 16:50:13 +0800

    first commit

---
+++ src/main/java/springAnno/EnableThreadPool.java
@@ -0,0 +1,17 @@
+package springAnno;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import tools.threadpool.ThreadPoolAutoConfiguration;
+
+@Retention(RetentionPolicy.RUNTIME)  // 运行时可用
+@Target({ElementType.METHOD, ElementType.TYPE})
+@ComponentScan(basePackageClasses = ThreadPoolAutoConfiguration.class)
+public @interface EnableThreadPool {
+
+}
---
+++ src/main/java/tools/threadpool/ThreadPoolAutoConfiguration.java
@@ -0,0 +1,10 @@
+package tools.threadpool;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("tools.threadpool")
+public class ThreadPoolAutoConfiguration {
+
+}
